cd95835ff17689fb5d442fc627cd8d3b87b1c426
hawk-tomy <me@mail.hawk-tomy.dev>
2024-05-11 16:15:43 +0900

    fix valid_local_strings length is zero

--- lib/type.py
+++ lib/type.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Literal, TypeGuard, get_args
+from typing import Literal, TypeGuard
 
 type LocaleString = Literal[  # type: ignore[valid-type]
     'AR',
@@ -39,7 +39,42 @@ type LocaleString = Literal[  # type: ignore[valid-type]
     'ZH',
 ]
 
-valid_locale_strings: tuple[LocaleString, ...] = get_args(LocaleString)
+valid_locale_strings: tuple[LocaleString, ...] = (
+    'AR',
+    'BG',
+    'CS',
+    'DA',
+    'DE',
+    'EL',
+    'EN',
+    'EN-GB',
+    'EN-US',
+    'ES',
+    'ET',
+    'FI',
+    'FR',
+    'HU',
+    'ID',
+    'IT',
+    'JA',
+    'KO',
+    'LT',
+    'LV',
+    'NB',
+    'NL',
+    'PL',
+    'PT',
+    'PT-BR',
+    'PT-PT',
+    'RO',
+    'RU',
+    'SK',
+    'SL',
+    'SV',
+    'TR',
+    'UK',
+    'ZH',
+)
 
 
 def is_valid_locale(locale: str) -> TypeGuard[LocaleString]:
